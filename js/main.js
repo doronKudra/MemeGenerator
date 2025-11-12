40054a08754e141b7ecf96da7e33c3840286a659
Doron Kudra <doron0505@gmail.com>
2025-11-12 12:05:43 +0200

    App Skeleton, Basic UI

---
+++ js/main.js
@@ -0,0 +1,37 @@
+'use strict'
+
+function onInit(){
+
+}
+
+function onResize(){
+
+}
+
+function onDown(event){
+
+}
+
+function onUp(event){
+
+}
+
+function onDrag(event){
+
+}
+
+function onTextChange(elTxt){
+
+}
+
+function onShareCanvas(elLink){
+
+}
+
+function onDownloadCanvas(elLink){
+
+}
+
+function toggleMenu() {
+    document.body.classList.toggle('menu-open')
+}
